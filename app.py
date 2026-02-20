df45174e12179158ec1d034ca17675faf3e1eb64
Davi-Pessoa <dp3867148@gmail.com>
2026-02-20 20:26:56 -0300

    Add Graficos.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import plotly.express as px
 from dataset import df
 from utils import format_number
+from graficos import grafico_map_estado , grafico_rec_mensal
+
 
 
 st.set_page_config(layout="wide")
@@ -16,5 +18,7 @@ with aba2:
     coluna1, coluna2 = st.columns(2)
     with coluna1:
         st.metric("Receita Total",format_number(df['Preço'].sum(), 'R$ '))
+        st.plotly_chart(grafico_map_estado, use_container_width=True)
     with coluna2:
         st.metric("Quantidade de Vendas", format_number(df.shape[0]))
+        st.plotly_chart(grafico_rec_mensal, use_container_width=True)   
--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 from dataset import df  
+import pandas as pd
 
-# Agrupamento por estado e soma da receita
+# 1-Dataframe para receita por estado
 
 df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
 df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
-print(df_rec_estado)
+#print(df_rec_estado)
+
+#2- Dataframe para receita mensal
+
+df_rec_mensal = df.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name()
+
+print(df_rec_mensal)
 
 
 #formatação de números
@@ -15,4 +24,4 @@ def format_number(value, prefix = ''):
         if value < 1000:
             return f"{prefix}{value:.2f}{unit}"
         value /= 1000
-    return f"{prefix}{value:.2f}Milhões"        
+    return f"{prefix}{value:.2f}Milhões"         
